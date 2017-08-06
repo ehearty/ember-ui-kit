554c163a21af3d8e7d0708c3cffefb32dea89528
Ming Liu <vmliu1@gmail.com>
2017-08-06 00:09:22 -0700

    Merge --fixed table components

--- addon/components/ui-table--fixed.js
+++
@@ -1,8 +0,0 @@
-import Ember from 'ember';
-import Table from './ui-table--v2';
-import layout from '../templates/components/ui-table--fixed';
-
-export default Table.extend({
-  classNames: 'ui-table--fixed',
-  layout
-});
--- addon/components/ui-tfoot--fixed.js
+++
@@ -1,8 +0,0 @@
-import Ember from 'ember';
-import TableFoot from './ui-tfoot';
-import layout from '../templates/components/ui-tfoot--fixed';
-
-export default TableFoot.extend({
-  classNames: 'ui-tfoot--fixed',
-  layout
-});
