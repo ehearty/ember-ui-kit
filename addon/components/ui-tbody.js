3e4c869cb9376deb0d7d7422c30c41132331e8ef
Ming Liu <vmliu1@gmail.com>
2017-08-14 18:26:28 -0700

    Implement tr hover

--- addon/components/ui-tbody.js
+++ addon/components/ui-tbody.js
@@ -3,5 +3,27 @@ import layout from '../templates/components/ui-tbody';
 
 export default Ember.Component.extend({
   classNames: 'ui-tbody',
-  layout
+  layout,
+
+  didInsertElement() {
+    this._super(...arguments);
+
+    this.$()
+      .on('mouseenter', '.ui-tr', evt => {
+        let index = Ember.$(evt.currentTarget).attr('data-row-index');
+
+        this.$(`[data-row-index=${index}]`).addClass('ui-tr--hover');
+      })
+      .on('mouseleave', '.ui-tr', evt => {
+        let index = Ember.$(evt.currentTarget).attr('data-row-index');
+
+        this.$(`[data-row-index=${index}]`).removeClass('ui-tr--hover');
+      });
+  },
+
+  willDestroyElement() {
+    this._super(...arguments);
+
+    this.$().off();
+  }
 });
