936bbde9be0a6043a74d698ee45c9c981109448f
Ming Liu <vmliu1@gmail.com>
2017-08-23 22:58:55 -0700

    Convert to use vertical collection

--- addon/components/ui-tr--each.js
+++ addon/components/ui-tr--each.js
@@ -15,94 +15,100 @@ export default Ember.Component.extend({
    */
   key: null,
 
-  // TODO flatten tree model
-  modelNormalized: Ember.computed.readOnly('model'),
+  didInsertElement() {
+    this._super(...arguments);
 
-  rowHeight: Ember.computed(function() {
-    return this.$('.ui-tr--measure .ui-tr').outerHeight(true);
-  }).readOnly(),
+    this.set('rowHeight', this.$('.ui-tr--measure .ui-tr').outerHeight(true));
+  },
 
-  bufferSize: Ember.computed('rowHeight', 'modelNormalized.length', function() {
-    let screenWidth = Math.max(document.documentElement.clientWidth, window.innerWidth || 0);
-    let screenHeight = Math.max(document.documentElement.clientHeight, window.innerHeight || 0);
-    let rowHeight = this.get('rowHeight');
-    let len = this.get('modelNormalized.length');
+  //// TODO flatten tree model
+  //modelNormalized: Ember.computed.readOnly('model'),
 
-    let computed = Math.ceil(Math.max(screenWidth, screenHeight) / rowHeight);
+  //rowHeight: Ember.computed(function() {
+  //  return this.$('.ui-tr--measure .ui-tr').outerHeight(true);
+  //}).readOnly(),
 
-    return Math.min(computed, len);
-  }),
-  bufferStart: 0,
-  buffer: Ember.computed('bufferSize', function() {
-    let size = this.get('bufferSize');
+  //bufferSize: Ember.computed('rowHeight', 'modelNormalized.length', function() {
+  //  let screenWidth = Math.max(document.documentElement.clientWidth, window.innerWidth || 0);
+  //  let screenHeight = Math.max(document.documentElement.clientHeight, window.innerHeight || 0);
+  //  let rowHeight = this.get('rowHeight');
+  //  let len = this.get('modelNormalized.length');
 
-    return Ember.A(Array.from({ length: size }).map(function(fill, index) {
-      return Ember.Object.create({
-        id: index
-      })
-    }));
-  }).readOnly(),
+  //  let computed = Math.ceil(Math.max(screenWidth, screenHeight) / rowHeight);
 
-  bufferContent: Ember.computed('modelNormalized.[]', 'bufferStart', 'bufferSize', function() {
-    let model = this.get('modelNormalized');
-    let start = this.get('bufferStart');
-    let size = this.get('bufferSize');
+  //  return Math.min(computed, len);
+  //}),
+  //bufferStart: 0,
+  //buffer: Ember.computed('bufferSize', function() {
+  //  let size = this.get('bufferSize');
 
-    return model.slice(start, start + size).map((item, index) => {
-      return {
-        model: item,
-        index: start + index
-      };
-    });
-  }).readOnly(),
+  //  return Ember.A(Array.from({ length: size }).map(function(fill, index) {
+  //    return Ember.Object.create({
+  //      id: index
+  //    })
+  //  }));
+  //}).readOnly(),
 
-  bufferInPlace: Ember.computed('buffer', 'bufferContent', function() {
-    let buffer = this.get('buffer');
-    let content = this.get('bufferContent');
-    let size = content.length;
+  //bufferContent: Ember.computed('modelNormalized.[]', 'bufferStart', 'bufferSize', function() {
+  //  let model = this.get('modelNormalized');
+  //  let start = this.get('bufferStart');
+  //  let size = this.get('bufferSize');
 
-    while (size--) {
-      buffer.objectAt(size).setProperties(content[size]);
-    }
+  //  return model.slice(start, start + size).map((item, index) => {
+  //    return {
+  //      model: item,
+  //      index: start + index
+  //    };
+  //  });
+  //}).readOnly(),
 
-    return buffer;
-  }).readOnly(),
+  //bufferInPlace: Ember.computed('buffer', 'bufferContent', function() {
+  //  let buffer = this.get('buffer');
+  //  let content = this.get('bufferContent');
+  //  let size = content.length;
+
+  //  while (size--) {
+  //    buffer.objectAt(size).setProperties(content[size]);
+  //  }
+
+  //  return buffer;
+  //}).readOnly(),
 
   block: Ember.computed(function() {
     return this.$().closest('[data-table-block]');
   }).readOnly(),
 
-  didInsertElement() {
-    this._super(...arguments);
+  //didInsertElement() {
+  //  this._super(...arguments);
 
-    let rowHeight = this.get('rowHeight');
-    let ns = this.get('elementId');
+  //  let rowHeight = this.get('rowHeight');
+  //  let ns = this.get('elementId');
 
-    this.$().closest('.ui-tbody').on(`scroll.${ns}`, evt => {
-      Ember.run(this, this.set, 'bufferStart', Math.floor(evt.target.scrollTop / rowHeight));
-    });
-  },
+  //  this.$().closest('.ui-tbody').on(`scroll.${ns}`, evt => {
+  //    Ember.run(this, this.set, 'bufferStart', Math.floor(evt.target.scrollTop / rowHeight));
+  //  });
+  //},
 
-  willDestroyElement() {
-    this._super(...arguments);
+  //willDestroyElement() {
+  //  this._super(...arguments);
 
-    let ns = this.get('elementId');
+  //  let ns = this.get('elementId');
 
-    this.$().closest('.ui-tbody').off(`.${ns}`);
-  },
+  //  this.$().closest('.ui-tbody').off(`.${ns}`);
+  //},
 
   didRender() {
     this._super(...arguments);
 
-    let length = this.get('modelNormalized.length');
+    let length = this.get('model.length');
     let rowHeight = this.get('rowHeight');
 
     let block = this.get('block');
     let start = this.get('bufferStart');
 
     block.css({
-      marginTop: rowHeight * start,
-      height: rowHeight * (length - start)
+      //marginTop: rowHeight * start,
+      height: rowHeight * length
     });
   }
 
