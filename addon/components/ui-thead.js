33f061ee4be480b62bf7ec4db247d501ff9f6f6d
Ming Liu <vmliu1@gmail.com>
2017-09-01 22:05:57 -0700

    Refactor to use x-resize

--- addon/components/ui-thead.js
+++ addon/components/ui-thead.js
@@ -5,19 +5,9 @@ import Styleable from '../mixins/styleable';
 
 import { layout as cssLayout } from '../utils/dom';
 
-/* global elementResizeDetectorMaker */
-let detector = elementResizeDetectorMaker({
-  strategy: 'scroll'
-});
-
 export default Ember.Component.extend(Styleable, {
   classNames: 'ui-thead',
   layout,
-
-  /**
-   * @attribute {} breakpoints
-   */
-  breakpoints: null,
 
   leafNodes: Ember.computed(function() {
     return this.$('[data-column-children="0"]');
@@ -30,33 +20,8 @@ export default Ember.Component.extend(Styleable, {
   didInsertElement() {
     this._super(...arguments);
 
-    //let leaves = this.get('leafNodes');
-    //let table = this.$().closest('.ui-table--v2');
-
-    detector.listenTo(this.$('.ui-thead__resizer').get(0), this.rerender = Ember.run.bind(this, this.rerender));
-
     this.$('.ui-sortable').on('sortupdate', (evt, { item }) => {
-      //let block = item.parentsUntil('.ui-thead', '[data-table-block]').attr('data-table-block');
       let ordered = item.closest('.ui-thead').find('.ui-th[data-column-id]');
-
-      //let targets = item.find('.ui-th').addBack().filter('[data-column-id]')
-      //  .map((index, element) => Ember.$(element).attr('data-column-id'))
-      //let after = Ember.$(ordered[ordered.index(ordered.filter(`[data-column-id="${targets.get(-1)}"]`)) + 1])
-      //  .attr('data-column-id');
-      //let selector = targets.toArray().map(target => `[data-column-id="${target}"]`).join();
-
-      // `target` is moved to before `after`
-
-      //table.find(`[data-table-block=${block}] .ui-tr`).each(function() {
-      //  let tr = Ember.$(this);
-
-      //  if (typeof after !== 'undefined') {
-      //    tr.find(selector).insertBefore(tr.find(`[data-column-id="${after}"]`));
-      //  }
-      //  else {
-      //    tr.find(selector).appendTo(tr);
-      //  }
-      //});
 
       ordered.attr('data-column-order', index => index);
 
@@ -122,8 +87,12 @@ export default Ember.Component.extend(Styleable, {
   willDestroyElement() {
     this._super(...arguments);
 
-    detector.removeListener(this.$('.ui-thead__resizer').get(0), this.rerender);
-
     this.$().off();
+  },
+
+  actions: {
+    rerender() {
+      this.rerender();
+    }
   }
 });
