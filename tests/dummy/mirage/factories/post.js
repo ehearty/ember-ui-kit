fc087356e5c9747b6ef1ca0637fec1b4e0f23d2f
Ming Liu <vmliu1@gmail.com>
2017-09-02 22:28:49 -0700

    Add lazy create mirage collection

--- tests/dummy/mirage/factories/post.js
+++ tests/dummy/mirage/factories/post.js
@@ -11,5 +11,27 @@ export default Factory.extend({
 
   content() {
     return faker.lorem.paragraphs();
+  },
+
+  afterCreate(post, server) {
+    let users = server.schema.users.all().models;
+    let tags = server.schema.tags.all().models;
+    let comments = server.createList('comment', faker.random.number(3));
+
+    post.authorId = users[faker.random.number(users.length - 1)].id;
+    post.tagIds = tags.slice(faker.random.number(tags.length - 1)).map(tag => tag.id);
+    post.commentIds = comments.map(comment => comment.id);
+
+    comments.forEach(comment => {
+      comment.postId = post.id;
+      comment.authorId = users[faker.random.number(users.length - 1)].id;
+
+      comment.post.save();
+      comment.author.save();
+    });
+
+    post.author.save();
+    post.tags.save();
+    post.comments.save();
   }
 });
