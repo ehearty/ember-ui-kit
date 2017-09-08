5fac2a93a54275c451e203e8243a9da67d85cea3
Ming Liu <vmliu1@gmail.com>
2017-09-07 20:11:38 -0700

    chrome removed?

--- testem.js
+++ testem.js
@@ -7,8 +7,18 @@ module.exports = {
     "dockcontainer": true
   },
   "launch_in_ci": [
-    "PhantomJS"
+    "Chrome"
   ],
   "launch_in_dev": [
-  ]
+  ],
+  "browser_args": {
+    "Chrome": [
+      "--headless",
+      "--disable-gpu",
+      "--remote-debugging-port=9222",
+      "--remote-debugging-address=0.0.0.0",
+      "--no-sandbox",
+      "--user-data-dir=/tmp"
+    ]
+  }
 };
